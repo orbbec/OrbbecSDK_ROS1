a065e096886a0d0cf80dd0af0407d684008c4708
Joe Dong <jian.dong@pobox.com>
2022-10-11 10:26:59 +0800

    add list device

--- src/list_devices_node.cpp
+++ src/list_devices_node.cpp
@@ -1,4 +1,13 @@
 #include <ros/ros.h>
 #include <orbbec_camera/types.h>
 
-int main() { return 0; }
+int main() {
+  auto context = std::make_unique<ob::Context>();
+  context->setLoggerSeverity(OBLogSeverity::OB_LOG_SEVERITY_NONE);
+  auto list = context->queryDeviceList();
+  for (size_t i = 0; i < list->deviceCount(); i++) {
+    auto serial = list->serialNumber(i);
+    ROS_INFO_STREAM("serial: " << serial);
+  }
+  return 0;
+}
