9cf2f8d0dcfaa9dd5c02fd64917bf7640bd21fe1
Joe Dong <jian.dong@pobox.com>
2022-10-11 15:41:20 +0800

    treat SDK as lfs

--- SDK/include/libobsensor/ObSensor.h
+++ SDK/include/libobsensor/ObSensor.h
@@ -1,26 +1,3 @@
-/* License: Apache 2.0. See LICENSE file in root directory.
-   Copyright(c) 2020  Orbbec Corporation. All Rights Reserved. */
-
-/**
- * \if English
- * \file ObSensor.h
- * \brief libobsensor C entrance
- * \else
- * \file ObSensor.h
- * \brief  libobsensor的C的入口
- * \endif
- *
- */
-#pragma once
-#include <libobsensor/h/Context.h>
-#include <libobsensor/h/Device.h>
-#include <libobsensor/h/Error.h>
-#include <libobsensor/h/Filter.h>
-#include <libobsensor/h/Frame.h>
-#include <libobsensor/h/ObTypes.h>
-#include <libobsensor/h/Pipeline.h>
-#include <libobsensor/h/Property.h>
-#include <libobsensor/h/RecordPlayback.h>
-#include <libobsensor/h/Sensor.h>
-#include <libobsensor/h/StreamProfile.h>
-#include <libobsensor/h/Version.h>
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ec339babe00ce5e6b6598320def99434b85ffc0c0999a908da529064d93678a
+size 722
