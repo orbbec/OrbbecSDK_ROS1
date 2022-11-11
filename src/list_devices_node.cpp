41e321db561c305f0ce6f226d750d779a0883060
Joe Dong <jian.dong@pobox.com>
2022-11-11 17:35:26 +0800

    fixed get serial

--- src/list_devices_node.cpp
+++ src/list_devices_node.cpp
@@ -6,7 +6,7 @@ int main() {
   context->setLoggerSeverity(OBLogSeverity::OB_LOG_SEVERITY_NONE);
   auto list = context->queryDeviceList();
   for (size_t i = 0; i < list->deviceCount(); i++) {
-    auto serial = list->serialNumber(i);
+    auto serial = list->getDevice(i)->getDeviceInfo()->serialNumber();
     ROS_INFO_STREAM("serial: " << serial);
   }
   return 0;
