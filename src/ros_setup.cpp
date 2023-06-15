478e538957c25e52ee1613131b09aad1c6027510
Joe Dong <jian.dong@pobox.com>
2023-06-15 09:20:17 +0800

    add uyuv to rgb888

--- src/ros_setup.cpp
+++ src/ros_setup.cpp
@@ -141,6 +141,9 @@ bool OBCameraNode::setupFormatConvertType(OBFormat type) {
     case OB_FORMAT_NV12:
       format_convert_filter_.setFormatConvertType(FORMAT_NV12_TO_RGB888);
       break;
+    case OB_FORMAT_UYVY:
+      format_convert_filter_.setFormatConvertType(FORMAT_UYVY_TO_RGB888);
+      break;
     default:
       return false;
   }
