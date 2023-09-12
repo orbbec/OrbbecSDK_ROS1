d08b805a46ac4183c5c013d986955524e88710ef
Joe Dong <jian.dong@pobox.com>
2023-09-12 11:46:10 +0800

    fix mising close sem

--- src/ob_camera_node_driver.cpp
+++ src/ob_camera_node_driver.cpp
@@ -1,18 +1,18 @@
 /*******************************************************************************
-* Copyright (c) 2023 Orbbec 3D Technology, Inc
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright (c) 2023 Orbbec 3D Technology, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 #include "orbbec_camera/ob_camera_node_driver.h"
 #include <fcntl.h>
 #include <unistd.h>
@@ -127,6 +127,11 @@ std::shared_ptr<ob::Device> OBCameraNodeDriver::selectDevice(
   }
 
   sem_t* device_sem = sem_open(DEFAULT_SEM_NAME.c_str(), O_CREAT, 0644, 1);
+  std::shared_ptr<int> device_sem_guard(nullptr, [&, device_sem](int*) {
+    if (device_sem != SEM_FAILED) {
+      sem_close(device_sem);
+    }
+  });
   if (device_sem == SEM_FAILED) {
     ROS_ERROR_STREAM("Failed to open semaphore");
     return nullptr;
