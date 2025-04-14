437cbf30e91ec2fda4d9bad105bc0b454a4781ac
jj <957713278@qq.com>
2025-04-14 13:40:42 +0800

    bump to v1.5.16

--- include/orbbec_camera/constants.h
+++ include/orbbec_camera/constants.h
@@ -23,7 +23,7 @@
 
 #define OB_ROS_MAJOR_VERSION 1
 #define OB_ROS_MINOR_VERSION 5
-#define OB_ROS_PATCH_VERSION 15
+#define OB_ROS_PATCH_VERSION 16
 
 #ifndef STRINGIFY
 #define STRINGIFY(arg) #arg
