c3a7f82773526857c8e0db1efb86c6d63d8bcb6b
slz <shenlanzhou@orbbec.com>
2026-07-02 11:22:39 +0800

    fix: set diagnostics_frequency default to 0.0 to disable diagnostics

--- include/orbbec_camera/ob_camera_node.h
+++ include/orbbec_camera/ob_camera_node.h
@@ -863,7 +863,7 @@ class OBCameraNode {
   ros::Timer lrm_obstacle_distance_timer_;
   ros::Publisher lrm_obstacle_distance_pub_;
   std::shared_ptr<diagnostic_updater::Updater> diagnostic_updater_ = nullptr;
-  double diagnostics_frequency_ = 1.0;
+  double diagnostics_frequency_ = 0.0;
   std::shared_ptr<std::thread> diagnostics_thread_ = nullptr;
   bool enable_laser_ = true;
   std::string align_mode_ = "HW";
